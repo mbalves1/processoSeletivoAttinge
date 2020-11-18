ee7f726f553865f6a18dd1c5859db54f200e16db
Murilo B Alves <61016975+mbalves1@users.noreply.github.com>
2020-11-18 16:09:24 -0300

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -3,13 +3,11 @@ const closeIcon = document.querySelector('.menuClose')
 
 const nav = document.querySelector('.mainNav')
 
-
     hamburgerIcon.addEventListener('click', () => {
         hamburgerIcon.classList.add('hide');
         closeIcon.classList.remove('hide');
         nav.classList.add('show')
-       
-       
+  
     })
 
     closeIcon.addEventListener('click', () => {
@@ -18,3 +16,29 @@ const nav = document.querySelector('.mainNav')
         nav.classList.remove('hide')
         nav.classList.remove('show')
     })
+
+
+
+    /*******************  Animação   ******************* */
+
+    const target = document.querySelectorAll('[data-anime]')
+    const animetionClass = 'animate';
+
+    function animeScroll(){
+        const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
+        target.forEach(function(element){
+            if((windowTop) > element.offsetTop){
+                element.classList.add(animetionClass)
+            } else  {
+                element.classList.remove(animetionClass)
+            }
+        })
+    }
+
+    animeScroll();
+
+    if(target.length){
+        window.addEventListener('scroll', function(){
+            animeScroll();
+        })
+    }
